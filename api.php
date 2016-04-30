f436535a72f7b1d822bbc4b6a2e89ab59141229a
copoer <coop@tuta.io>
2016-04-30 19:32:13 +0000

    Q and String escape added

--- api.php
+++ api.php
@@ -3,43 +3,58 @@
 require "../db/dbconn.php";
 
 if(isset($_GET['type']) && $_GET['pass'] == "wordpass") {
-	/*
-	$id = $_GET['id'];
-	$type = $_GET['type'];
-	$name = $_GET['name'];
-	$back = $_GET['back'];
-	$front = $_GET['front'];
-	 */
+	
+	/* "im trying atlest" */
+	$id = $mysql_escape_string($_GET['id']);
+	$type = mysql_escape_string($_GET['type']);
+	$name = mysql_escape_string($_GET['name']);
+	$back = mysql_escape_string($_GET['back']);
+	$front = mysql_escape_string($_GET['front']);
+	$location = mysql_escape_string($_GET['location']);
+	$amount = mysql_escape_string($_GET['amount']);
+
+	if(isset($_GET['base'])) {
+		$base = mysql_escape_string($_GET['base']);
+	} else {
+		$base = "stock";
+	}
+
 
 	if($_GET['type'] == "refresh") {
 		$Jam = new Jam();
-		$Jam->query();
+		$Jam->query($base);
 	}
-	
+	if($_GET['type'] == "del") {
+		$Jam = new Jam();
+		$Jam->del($base,$id);
+	}
 	if($_GET['type'] == "update") {
 		$Jam = new Jam();
-		$Jam->update($_GET['id'],$_GET['back'],$_GET['front']);
+		$Jam->update($base,$id,$back,$front);
 	}
-
 	if($_GET['type'] == "insert") {
 		$Jam = new Jam();
-		$Jam->push($_GET['name'],$_GET['back'],$_GET['front']);
+		$Jam->insert($base,$name,$back,$location);
+	}
+
+	
+	/* for q */
+	if($_GET['type'] == "push") {
+		$Jam = new Jam();
+		$Jam->push($id,$amount);
+	}
+	if($_GET['type'] == "move") {
+		$Jam = new Jam();
+		$Jam->move($id);
 	}
 
 }
 
 class Jam {
-
-	function push($name,$back,$front) {
+	/* table wide */
+	function query($base) {
 		require "../db/dbconn.php";
-		$query = "INSERT INTO stock(name,back,front) VALUES ('$name','$back','$front')";
-		$result = $mysqli->query($query);
-		$this->query();
-	}
-
-	function query() {
-		require "../db/dbconn.php";
-		$query = "select * from stock";
+		$query = "select * from $base";
 		$result = $mysqli->query($query);
 		$result->data_seek(0);
 		$list = array(); 
@@ -48,12 +63,69 @@ class Jam {
 		}
 		echo json_encode($list);
 	}
-
-	function update($id,$back,$front) {
+	function del($base,$id){
 		require "../db/dbconn.php";
-		$query = "UPDATE stock SET back='$back',front='$front' WHERE id='$id'";
+		$query = "DELETE FROM $base WHERE id=$id";
 		$result = $mysqli->query($query);
-		$this->query();
+	}
+
+	/* for the q */
+	function push($id,$amount) {
+		require "../db/dbconn.php";
+		$query = "select * from stock where id=$id";
+		$result = $mysqli->query($query);
+		$result->data_seek(0);
+		while ($row = $result->fetch_assoc()) {
+			$name = $row['name'];
+			$location = $row['location'];
+			$back = $row['back'];
+			$front = $row['front'];
+		}
+		$query = "INSERT INTO q(stockid,name,location,amount) VALUES ('$id','$name','$location','$amount')";
+		$result = $mysqli->query($query);
+		$this->query('q');
+
+	}
+	function move($id) {
+		require "../db/dbconn.php";
+		$query = "select * from q where id=$id";
+		$result = $mysqli->query($query);
+		$result->data_seek(0);
+		while ($row = $result->fetch_assoc()) {
+			$sid = $row['stockid'];
+			$amount = $row['amount'];
+		}
+		$this->del("q",$id);
+		$query = "select * from stock where id=$sid";
+		$result = $mysqli->query($query);
+		$result->data_seek(0);
+		while ($row = $result->fetch_assoc()) {
+			$id = $row['stockid'];
+ 			$back = $row['back'];
+			$front = $row['front'];
+
+		}
+		if($back < $amount){
+			echo "ERROR: That amount can not be found in stock";
+			die();
+		}
+		$back = $back - $amount;
+		$front = $front + $amount;
+		$this->update("stock",$sid,$back,$front);
+	}
+
+	/* for the stock */
+	function insert($base,$name,$back,$location) {
+		require "../db/dbconn.php";
+		$query = "INSERT INTO $base(name,back,location) VALUES ('$name','$back','$location')";
+		$result = $mysqli->query($query);
+		$this->query($base);
+	}
+	function update($base,$id,$back,$front) {
+		require "../db/dbconn.php";
+		$query = "UPDATE $base SET back='$back',front='$front' WHERE id='$id'";
+		$result = $mysqli->query($query);
+		$this->query($base);
 	}
 
 }
