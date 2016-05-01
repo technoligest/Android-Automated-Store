e45514aa80f35a87e42ed919fb8e7fe0a5a5d0e9
copoer <coop@tuta.io>
2016-05-01 01:31:08 +0000

    Added Buyer Script

---
+++ cron/buyer.php
@@ -0,0 +1,39 @@
+<?php
+
+function query() {
+	require "../../db/dbconn.php";
+	$query = "select * from stock";
+	$result = $mysqli->query($query);
+	$result->data_seek(0);
+	$list = array(); 
+	while ($row = $result->fetch_assoc()) {
+		$list[] = $row;
+	}
+	return $list;
+}
+
+function update($base,$id,$back,$front) {
+	require "../../db/dbconn.php";
+	$query = "UPDATE $base SET back='$back',front='$front' WHERE id='$id'";
+	$result = $mysqli->query($query);
+}
+
+$data = query();
+foreach($data as $key => $value)
+{
+	foreach($value as $key => $v) {
+		$id = $value['id'];
+		$front = $value['front'];
+		$back = $value['back'];
+		break;
+	}
+	$front = $front - 1;
+	update("stock",$id,$back,$front);
+}
+/*
+foreach($a as $data) {
+	echo $data[$a];
+	$a++;
+
+}
+ */
